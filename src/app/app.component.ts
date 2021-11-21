1e007da676698906bfc412db27d98ff78bf6c6f7
Edwike12 <nyaunchoedwike@gmail.com>
2021-11-21 23:16:49 +0300

    created another component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,9 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-Quote:string[];
+Quote:Quote[]=[
+  {id:'1',Quote:'If you are always trying to be normal you will never know how amazing you can be'},
+  {id:'2',Quote:'Most of the important things in the world have been accomplished by people who kept on trying when their seemed to be no hope'}
+];
 
-
-constructor(){
-  this.Quote=['If you are always trying to be normal, you will never know how amazing you can be']
-}
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,3 @@
-export class Quote {
-  
+export class Quote{
+    
 }
