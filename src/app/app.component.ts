456132fbdf58f910ba245a83a4e27b2b92916f0a
Edwike12 <nyaunchoedwike@gmail.com>
2021-11-24 18:47:42 +0300

    added description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
 
 constructor() { 
   this.Quote=['Watch finding Nemo','Buy Cookies','Get new Phone Case','Get Dog Food','Solve math homework','Plot my world domination plan']
+  
 }
 
 ngOnInit() {
