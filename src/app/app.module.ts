db8c5ff7e2e86609a22893dcea9f53e34b5b7a34
Edwike12 <nyaunchoedwike@gmail.com>
2021-11-26 11:54:36 +0300

    created a votes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { FormsModule } from '@angular/forms';
+import { VotesComponent } from './votes/votes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    VotesComponent
   ],
   imports: [
     BrowserModule,
